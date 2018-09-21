0aee665639b92a7e05dc4500f50b6cd21789c561
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-09-21 10:50:43 +0800

    Add StackDetail and unit test

---
+++ model/StackDetail.ts
@@ -0,0 +1,51 @@
+import AWS = require('aws-sdk');
+import CloudFormation = require('aws-sdk/clients/cloudformation');
+
+export class StackDetail {
+    public constructor() {
+        AWS.config.update({region: process.env.AWS_DEFAULT_REGION});
+    }
+
+    public async getStackOutputs(stackName: string): Promise<{[key: string]: string} | null> {
+        let output: {[key: string]: string} | null = null;
+
+        const describeStacksOutput: CloudFormation.DescribeStacksOutput = await this.describeStacks(stackName);
+        if (!describeStacksOutput.Stacks) {
+            return null;
+        }
+
+        for (let stack of describeStacksOutput.Stacks) {
+            if (stack.StackName === stackName) {
+
+                output = {};
+                for (let stackOutput of stack.Outputs) {
+                    output[stackOutput.OutputKey] = stackOutput.OutputValue;
+                }
+
+                break;
+            }
+        }
+
+        return output;
+    }
+
+    public async describeStacks(stackname: string): Promise<CloudFormation.DescribeStacksOutput> {
+        return new Promise<CloudFormation.DescribeStacksOutput>((resolve, reject) => {
+
+            const describeStacksInput: CloudFormation.DescribeStacksInput = {
+                StackName: stackname
+            };
+
+            const cloudFormation = new CloudFormation();
+            cloudFormation.describeStacks(describeStacksInput, (err, data: CloudFormation.DescribeStacksOutput) => {
+                if (err) {
+                    console.log(err);
+
+                    return reject(err);
+                }
+
+                return resolve(data);
+            });
+        });
+    }
+}
---
+++ test/model/StackDetail.test.ts
@@ -0,0 +1,102 @@
+import {expect} from 'chai';
+
+import * as sinon from 'sinon';
+
+import {StackDetail} from '../../model/StackDetail';
+
+describe('StackDetail', () => {
+    let sandbox: any;
+    let describeStacksOutput: any;
+
+    before(() => {
+        sandbox = sinon.createSandbox();
+
+        describeStacksOutput = {
+            "ResponseMetadata":{
+                "RequestId":"95c33e7f-bcc5-11e8-b22d-c174831f5cab"
+            },
+            "Stacks":[
+                {
+                    "StackId":"arn:aws:cloudformation:ap-southeast-1:123456789012:stack/aws-lambda-error-rate-dev/5fa96c40-b65e-11e8-b89d-503a138dba36",
+                    "StackName":"aws-lambda-error-rate-dev",
+                    "Description":"The AWS CloudFormation template for this Serverless application",
+                    "Parameters":[
+        
+                    ],
+                    "CreationTime":"2018-09-12T07:35:08.299Z",
+                    "LastUpdatedTime":"2018-09-18T03:25:06.376Z",
+                    "RollbackConfiguration":{
+        
+                    },
+                    "StackStatus":"UPDATE_COMPLETE",
+                    "DisableRollback":false,
+                    "NotificationARNs":[
+        
+                    ],
+                    "Capabilities":[
+                        "CAPABILITY_IAM",
+                        "CAPABILITY_NAMED_IAM"
+                    ],
+                    "Outputs":[
+                        {
+                            "OutputKey":"ErrorLambdaFunctionQualifiedArn",
+                            "OutputValue":"arn:aws:lambda:ap-southeast-1:123456789012:function:aws-lambda-error-rate-dev-error:13",
+                            "Description":"Current Lambda function version"
+                        },
+                        {
+                            "OutputKey":"LambdaErrorSnsTopicArn",
+                            "OutputValue":"arn:aws:sns:ap-southeast-1:123456789012:aws-lambda-error-rate-dev-LambdaErrorSnsTopic-ZU96UUZ1U7SX"
+                        },
+                        {
+                            "OutputKey":"LambdaErrorAlarmArn",
+                            "OutputValue":"arn:aws:cloudwatch:ap-southeast-1:123456789012:alarm:LambdaErrorAlarm"
+                        },
+                        {
+                            "OutputKey":"ErrorRateLambdaFunctionQualifiedArn",
+                            "OutputValue":"arn:aws:lambda:ap-southeast-1:123456789012:function:aws-lambda-error-rate-dev-errorRate:14",
+                            "Description":"Current Lambda function version"
+                        },
+                        {
+                            "OutputKey":"ServerlessDeploymentBucketName",
+                            "OutputValue":"aws-lambda-error-rate-de-serverlessdeploymentbuck-8a5xb9btbg82"
+                        },
+                        {
+                            "OutputKey":"LambdaErrorRateSecurityGroup",
+                            "OutputValue":"sg-0b72fe1e138ecede2"
+                        }
+                    ],
+                    "Tags":[
+                        {
+                            "Key":"STAGE",
+                            "Value":"dev"
+                        }
+                    ],
+                    "EnableTerminationProtection":false
+                }
+            ]
+        };
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should get stack outputs', async () => {
+        const stackDetail = new StackDetail();
+
+        sandbox.stub(StackDetail.prototype, 'describeStacks').callsFake(() => {
+            return describeStacksOutput;
+        });
+
+        const stackOutputs: {[key: string]: string} = await stackDetail.getStackOutputs('aws-lambda-error-rate-dev');
+
+        expect(stackOutputs).to.deep.include({
+            "ErrorLambdaFunctionQualifiedArn": "arn:aws:lambda:ap-southeast-1:123456789012:function:aws-lambda-error-rate-dev-error:13",
+            "LambdaErrorSnsTopicArn": "arn:aws:sns:ap-southeast-1:123456789012:aws-lambda-error-rate-dev-LambdaErrorSnsTopic-ZU96UUZ1U7SX",
+            "LambdaErrorAlarmArn": "arn:aws:cloudwatch:ap-southeast-1:123456789012:alarm:LambdaErrorAlarm",
+            "ErrorRateLambdaFunctionQualifiedArn": "arn:aws:lambda:ap-southeast-1:123456789012:function:aws-lambda-error-rate-dev-errorRate:14",
+            "ServerlessDeploymentBucketName": "aws-lambda-error-rate-de-serverlessdeploymentbuck-8a5xb9btbg82",
+            "LambdaErrorRateSecurityGroup": "sg-0b72fe1e138ecede2"
+        });
+    });
+});
