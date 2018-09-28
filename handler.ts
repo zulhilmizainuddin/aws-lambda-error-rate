0e4d0e986f8fc589713390b129004ab8e3f95ede
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-09-28 14:12:12 +0800

    Draft for errorRate handler

--- handler.ts
+++ handler.ts
@@ -1,18 +1,228 @@
-import {APIGatewayEvent, Context, Callback} from 'aws-lambda';
+import {APIGatewayEvent, SNSEvent, Context, Callback} from 'aws-lambda';
 
-module.exports.errorRate = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+import {AlarmState} from './model/AlarmState';
+import {AlarmStatus, AlarmNotification} from './model/AlarmNotification';
+import {ErrorRateThreshold} from './model/ErrorRateThreshold';
+import {EventRuleInput} from './model/EventRule';
+import {EventScheduler} from './model/EventScheduler';
+import {IncidentWebhook} from './model/IncidentWebhook';
+import {MetricErrorRate, MetricData} from './model/MetricData';
+import {MetricTimeRange, MetricTimeRangeHelper} from './model/MetricTimeRange';
+import {NotificationFlag} from './model/NotificationFlag';
+import {PagerTreeWebhook} from './model/PagerTreeWebhook';
+import {StackDetail} from './model/StackDetail';
 
-  const result = {
-    message: 'Go Serverless v1.0! Your function executed successfully!',
-    event
-  };
+import {Duration} from './enum/Duration';
+import {Period} from './enum/Period';
+import {RateExpression} from './enum/RateExpression';
+import {Threshold} from './enum/Threshold';
 
-  callback(null, result);
+const STACK_NAME: string = 'aws-lambda-error-rate-dev';
+const ERROR_FUNCTION_NAME: string = 'aws-lambda-error-rate-dev-error';
+const ERROR_RATE_FUNCTION_NAME: string = 'aws-lambda-error-rate-dev-errorRate';
+const ERROR_RATE_FUNCTION_ARN: string = 'arn:aws:lambda:ap-southeast-1:123456789012:function:aws-lambda-error-rate-dev-errorRate';
+const PAGER_TREE_URL: string = 'https://api.pagertree.com/integration/int_Hk50sQvKm';
+const INCIDENT_FLAG_BUCKET_NAME: string = 'aws-lambda-error-rate-de-errorratenotificationfla-ea2sgss74yj3';
+
+module.exports.errorRate = async (event: any, context: Context, callback: Callback) => {
+
+    try {
+        console.log('event:');
+        console.log(JSON.stringify(event));
+
+        console.log('context:');
+        console.log(JSON.stringify(context));
+
+        const stackDetail = new StackDetail();
+        const stackOutput: { [key: string]: string } | null = await stackDetail.getStackOutputs(STACK_NAME);
+        if (!stackOutput) {
+            return callback('Failed to get stack outputs');
+        }
+
+        if (event.Records) {
+            console.log('event is SNSEvent');
+
+            event = event as SNSEvent;
+
+            const alarmNotification = new AlarmNotification();
+            const alarmStatus: AlarmStatus | null = alarmNotification.extractAlarmStatus(event);
+
+            console.log('alarmStatus:');
+            console.log(JSON.stringify(alarmStatus));
+
+            if (!alarmStatus) {
+                return callback('Failed to extract alarm status');
+            }
+
+            // const metricTimeRange: MetricTimeRange = MetricTimeRangeHelper.calculate(alarmStatus.stateChangeTime, Duration.ThreeHundredSeconds);
+
+            // console.log('metricTimeRange:');
+            // console.log(JSON.stringify(metricTimeRange));
+
+            // const metricData = new MetricData();
+            // const metricErrorRates: MetricErrorRate[] = await metricData.getMetricErrorRates(ERROR_FUNCTION_NAME, metricTimeRange, Period.SixtySeconds);
+
+            // console.log('metricErrorRates:');
+            // console.log(JSON.stringify(metricErrorRates));
+
+            // const errorRateThreshold = new ErrorRateThreshold();
+            // const isExceedThreshold: boolean = errorRateThreshold.isExceedThreshold(metricErrorRates, Threshold.OnePercent, Duration.ThreeHundredSeconds, Period.SixtySeconds);
+
+            // console.log('isExceedThreshold:');
+            // console.log(isExceedThreshold);
+
+            // if (isExceedThreshold) {
+
+            //     const incidentWebhook: IncidentWebhook = new PagerTreeWebhook(
+            //         PAGER_TREE_URL,
+            //         alarmStatus.stateChangeTime,
+            //         alarmStatus.alarmName,
+            //         `Error percentage > ${Threshold.OnePercent}% for at least ${Duration.ThreeHundredSeconds} seconds`);
+
+            //     const isCreateIncidentSuccess: boolean = await incidentWebhook.createIncident();
+
+            //     console.log('isCreateIncidentSuccess:');
+            //     console.log(isCreateIncidentSuccess);
+
+            //     if (!isCreateIncidentSuccess) {
+            //         return callback('Failed to create incident');
+            //     }
+
+            //     const notificationFlag = new NotificationFlag();
+            //     const isPutIncidentFlagSuccess: boolean = await notificationFlag.putFlag(INCIDENT_FLAG_BUCKET_NAME, alarmStatus.alarmName);
+
+            //     console.log('isPutIncidentFlagSuccess:');
+            //     console.log(isPutIncidentFlagSuccess);
+
+            //     if (!isPutIncidentFlagSuccess) {
+            //         return callback('Failed to put incident flag to bucket');
+            //     }
+            // }
+
+            const eventScheduler = new EventScheduler();
+            const isCreateEventSuccess: boolean = await eventScheduler.createEvent(alarmStatus, RateExpression.OneMinute, ERROR_RATE_FUNCTION_ARN, ERROR_RATE_FUNCTION_NAME);
+
+            console.log('isCreateEventSuccess:');
+            console.log(isCreateEventSuccess);
+
+            if (!isCreateEventSuccess) {
+                return callback('Failed to create scheduled event');
+            }
+
+        } else {
+            console.log('event is EventRuleInput');
+            // event = event as EventRuleInput;
+
+            // const castedEvent: EventRuleInput = event as EventRuleInput;
+
+            const alarmStatus: AlarmStatus = event.alarmStatus as AlarmStatus;
+
+            const alarmState = new AlarmState();
+            const activeAlarmState: string = await alarmState.getState(alarmStatus.alarmName);
+
+            console.log('activeAlarmState:');
+            console.log(activeAlarmState);
+
+            if (activeAlarmState === 'OK') {
+                const notificationFlag = new NotificationFlag();
+                const isIncidentFlagExist: boolean = await notificationFlag.getFlag(INCIDENT_FLAG_BUCKET_NAME, alarmStatus.alarmName);
+
+                console.log('isIncidentFlagExist:');
+                console.log(isIncidentFlagExist);
+
+                if (isIncidentFlagExist) {
+                    const incidentWebhook: IncidentWebhook = new PagerTreeWebhook(PAGER_TREE_URL, alarmStatus.stateChangeTime);
+                    const isResolveIncidentSuccess: boolean = await incidentWebhook.resolveIncident();
+
+                    console.log('isResolveIncidentSuccess:');
+                    console.log(isResolveIncidentSuccess);
+
+                    if (!isResolveIncidentSuccess) {
+                        return callback('Failed to resolve incident');
+                    }
+
+                    const isDeleteIncidentFlagSuccess: boolean = await notificationFlag.deleteFlag(INCIDENT_FLAG_BUCKET_NAME, alarmStatus.alarmName);
+
+                    console.log('isDeleteIncidentFlagSuccess:');
+                    console.log(isDeleteIncidentFlagSuccess);
+
+                    if (!isDeleteIncidentFlagSuccess) {
+                        return callback('Failed to delete incident flag');
+                    }
+                }
+
+                const eventScheduler = new EventScheduler();
+                const isDeleteEventSuccess: boolean = await eventScheduler.deleteEvent(alarmStatus.alarmName, ERROR_RATE_FUNCTION_NAME);
+
+                console.log('isDeleteEventSuccess:');
+                console.log(isDeleteEventSuccess);
+
+                if (!isDeleteEventSuccess) {
+                    return callback('Failed to delete scheduled event');
+                }
+            } else {
+                const metricTimeRange: MetricTimeRange = MetricTimeRangeHelper.calculate(new Date().toISOString(), Duration.ThreeHundredSeconds);
+
+                console.log('metricTimeRange:');
+                console.log(JSON.stringify(metricTimeRange));
+
+                const metricData = new MetricData();
+                const metricErrorRates: MetricErrorRate[] = await metricData.getMetricErrorRates(ERROR_FUNCTION_NAME, metricTimeRange, Period.SixtySeconds);
+
+                console.log('metricErrorRates:');
+                console.log(JSON.stringify(metricErrorRates));
+
+                const errorRateThreshold = new ErrorRateThreshold();
+                const isExceedThreshold: boolean = errorRateThreshold.isExceedThreshold(metricErrorRates, Threshold.OnePercent, Duration.ThreeHundredSeconds, Period.SixtySeconds);
+
+                console.log('isExceedThreshold:');
+                console.log(isExceedThreshold);
+
+                if (isExceedThreshold) {
+
+                    const incidentWebhook: IncidentWebhook = new PagerTreeWebhook(
+                        PAGER_TREE_URL,
+                        alarmStatus.stateChangeTime,
+                        alarmStatus.alarmName,
+                        `Error percentage > ${Threshold.OnePercent}% for at least ${Duration.ThreeHundredSeconds} seconds`);
+
+                    const isCreateIncidentSuccess: boolean = await incidentWebhook.createIncident();
+
+                    console.log('isCreateIncidentSuccess:');
+                    console.log(isCreateIncidentSuccess);
+
+                    if (!isCreateIncidentSuccess) {
+                        return callback('Failed to create incident');
+                    }
+
+                    const notificationFlag = new NotificationFlag();
+                    const isPutIncidentFlagSuccess: boolean = await notificationFlag.putFlag(INCIDENT_FLAG_BUCKET_NAME, alarmStatus.alarmName);
+
+                    console.log('isPutIncidentFlagSuccess:');
+                    console.log(isPutIncidentFlagSuccess);
+
+                    if (!isPutIncidentFlagSuccess) {
+                        return callback('Failed to put incident flag to bucket');
+                    }
+                }
+            }
+        }
+
+        const result = {
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            event
+        };
+
+        return callback(null, result);
+
+    } catch (ex) {
+        return callback(ex);
+    }
 };
 
 module.exports.error = (event: APIGatewayEvent, context: Context, callback: Callback) => {
-  
-  const error = new Error('Something went wrong')
 
-  callback(error);
+    const error = new Error('Something went wrong')
+
+    return callback(error);
 };
